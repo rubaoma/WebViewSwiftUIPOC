5df7ab7ef2d4b570cbb69863a21debfae3e63ef3
Rubens <rubao.ma@gmail.com>
2025-04-04 14:52:34 -0300

    Initial Commit

---
+++ WebViewPOC/WebViewPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewPOCApp.swift
+//  WebViewPOC
+//
+//  Created by Rubens Moura Augusto on 04/04/25.
+//
+
+import SwiftUI
+
+@main
+struct WebViewPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WebViewPOCTests/WebViewPOCTests.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewPOCTests.swift
+//  WebViewPOCTests
+//
+//  Created by Rubens Moura Augusto on 04/04/25.
+//
+
+import Testing
+@testable import WebViewPOC
+
+struct WebViewPOCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
